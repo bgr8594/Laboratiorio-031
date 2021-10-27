61cb0678e2fb2abf46c5fd62b275cb2b7dd9514c
Danielacarranza <danielacarcam@gmail.com>
2021-10-26 21:30:05 -0500

    Practica10, Agregar consulta snapshotChanges, Editar y eliminar en lugares/destinos

--- ionic-basic/src/app/destinos/destinos.page.ts
+++ ionic-basic/src/app/destinos/destinos.page.ts
@@ -12,24 +12,50 @@ export class DestinosPage implements OnInit {
   lugar: Lugar = new Lugar();
   destinos: any[] = [];
   ionicForm: FormGroup;
+  estado: string ="Alta destino";
+  editando: boolean= false;
   constructor(private lugarService: LugaresService,
     private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.buildForm();
-    this.lugarService.getLugares(this.destinos);
+    this.lugarService.getLugaresChanges().subscribe(resp => {
+      this.destinos = resp.map((e: any) => {
+        return {
+          id: e.payload.doc.id,
+          nombre: e.payload.doc.data().nombre
+        }
+      });
+    }, error => {
+      console.error(error);
+    });
   }
 
   altaLugar(){
     this.lugarService.altaLugar(this.lugar);
-    this.lugarService.getLugares(this.destinos);
     this.ionicForm.reset();
   }
 
   submitForm(){
     if(this.ionicForm.valid){
-      this.lugar.nombre = this.ionicForm.get('nombre').value;
-      this.altaLugar();
+      if(!this.editando){
+        this.lugar.nombre = this.ionicForm.get('nombre').value;
+        this.lugarService.altaLugar(this.lugar).then((e:any)=>{
+          this.ionicForm.reset();
+        }).catch(e=>{
+          console.error(e);
+        });        
+      } else{
+        this.lugar.nombre = this.ionicForm.get('nombre').value;
+        this.lugarService.updateLugares(this.lugar.id, this.lugar).then(e=>{
+          this.editando= false;
+          this.estado = "Alta destino";
+          this.lugar = new Lugar();
+          this.ionicForm.reset();
+        }).catch(e=>{
+          console.error(e);
+        });
+      }
     }
   }
 
@@ -44,4 +70,24 @@ export class DestinosPage implements OnInit {
 			this.ionicForm.controls[controlName].hasError(errorName) &&
 			this.ionicForm.controls[controlName].touched;
 	}
+  editarLugar(id: any, lugar: any) {
+    this.editando = true;
+    this.lugar = lugar;
+    this.estado = "Editar el lugar";
+    this.ionicForm.get('nombre').setValue(lugar.nombre);
+  }
+
+  eliminarLugar(id: any) {
+    this.estado = "Alta destino";
+    this.editando = false;
+    this.ionicForm.reset();
+    this.lugarService.deleteLugar(id);
+  }
+
+  cancelarEdicion(){
+    this.estado = "Alta destino";
+    this.editando = false;
+    this.ionicForm.reset();
+    this.lugar = new Lugar();
+  }
 }
