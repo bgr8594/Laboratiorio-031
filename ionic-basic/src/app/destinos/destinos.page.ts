9169009d2485a7b3910e0eb10b8b5d9021854d2d
AxelHdzB <axel.hernandezbt@uanl.edu.mx>
2021-11-13 13:39:27 -0600

    Preparacion para despliege en apk android sin api en linea

--- ionic-basic/src/app/destinos/destinos.page.ts
+++ ionic-basic/src/app/destinos/destinos.page.ts
@@ -25,7 +25,17 @@ export class DestinosPage implements OnInit {
   ngOnInit() {
     this.buildForm();
     this.getPosition();    
-    this.getLugares();
+    //this.getLugares();
+    this.lugarService.getLugaresChanges().subscribe(resp => {
+      this.destinos = resp.map((e: any) => {
+        return {
+          id: e.payload.doc.id,
+          nombre: e.payload.doc.data().nombre
+        }
+      });
+    }, error => {
+      console.error(error);
+  });
   }
 
   getLugares(){
@@ -38,33 +48,22 @@ export class DestinosPage implements OnInit {
 
   submitForm(){
     if(this.ionicForm.valid){
-      this.lugar.nombre = this.ionicForm.get('nombre').value;
-      this.lugar.latitud = this.latitud;
-      this.lugar.longitud = this.longitud;
       if(!this.editando){
-        this.lugarService.altaLugarApi(this.lugar).subscribe((response: any)=>{
-          if(response){
-            this.ionicForm.reset();
-            this.getLugares();
-          } else{
-            this.errorProceso();
-          }
-        }, error=>{
-          console.error(error);
-        })
+        this.lugar.nombre = this.ionicForm.get('nombre').value;
+        this.lugarService.altaLugar(this.lugar).then((e:any)=>{
+          this.ionicForm.reset();
+        }).catch(e=>{
+          console.error(e);
+        });
       } else{
-        this.lugarService.editarLugarApi(this.lugar.id, this.lugar).subscribe((resposne: any)=>{
-          if(resposne){
-            this.editando= false;
-            this.estado = "Alta destino";
-            this.lugar = new Lugar();
-            this.ionicForm.reset();
-            this.getLugares();
-          } else{
-            this.errorProceso();
-          }
-        }, error=>{
-          console.error(error);
+        this.lugar.nombre = this.ionicForm.get('nombre').value;
+        this.lugarService.updateLugares(this.lugar.id, this.lugar).then(e=>{
+          this.editando= false;
+          this.estado = "Alta destino";
+          this.lugar = new Lugar();
+          this.ionicForm.reset();
+        }).catch(e=>{
+          console.error(e);
       });
     }
   }
@@ -82,7 +81,7 @@ hasError: any = (controlName: string, errorName: string) => {
     this.ionicForm.controls[controlName].touched;
 }
 
-
+/*
 editarLugar(id: any, lugar: any) {
   this.editando = true;
   this.lugar = lugar;
@@ -111,55 +110,41 @@ editarLugar(id: any, lugar: any) {
     this.ionicForm.reset();
     this.lugar = new Lugar();
   }
+  
+ */
+  editarLugar(id: any, lugar: any) {
+    this.editando = true;
+    this.lugar = lugar;
+    this.estado = "Editar el lugar";
+    this.ionicForm.get('nombre').setValue(lugar.nombre);
+  }
 
+  eliminarLugar(id: any) {
+    this.estado = "Alta destino";
+    this.editando = false;
+    this.ionicForm.reset();
+    this.lugarService.deleteLugar(id);
+  }
+
+  cancelarEdicion(){
+    this.estado = "Alta destino";
+    this.editando = false;
+    this.ionicForm.reset();
+    this.lugar = new Lugar();
+  }
   getPosition(): Promise<any> {
-    return new Promise((resolve: any, reject: any): any=>{
-      navigator.geolocation.getCurrentPosition((resp: any)=>{
-        this.latitud = resp.coords.latitude;
-        this.longitud = resp.coords.longitude;
-      },
-      (err:any)=> {
-        if( err.code === 1) {
-          alert('Favor de activar la geolocalizacion en tu navegador y recargar la pantalla.');
-        }
-        this.latitud = null;
-        this.longitud = null;
-      },{timeout: 5000, enableHighAccuracy: true});
-    });
-  }
-
-  errorProceso(){
-    alert("Ocurrio un error en el proceso");
-  }
-
-  async addDirection(){
-    let positionInput: any = {
-      lat: -2.898116,
-      lng: -78.99958149999999
-    };
-    if(this.latitud !== null){
-      positionInput.lat = this.latitud;
-      positionInput.lng = this.longitud;
-    }
-
-
-    const modalAdd = await this.modalController.create({
-      component: GooglemapsComponent,
-      mode: 'ios',
-      swipeToClose: true,
-      componentProps: {position: positionInput} 
-    });
-
-    await modalAdd.present();
-
-    const {data} = await modalAdd.onWillDismiss();
-
-    if(data){
-      console.log('data->', data);
-      //this.cli
-      this.longitud = data.pos.lng;
-      this.latitud = data.pos.lat;
-      console.log('datos de ubiciacion actualizados, latitud: '+this.latitud+' \nlongitud:'+this.longitud);
-    }
-  }
+		return new Promise((resolve: any, reject: any): any => {
+			navigator.geolocation.getCurrentPosition((resp: any) => {
+				this.latitud = resp.coords.latitude;
+				this.longitud = resp.coords.longitude;
+			},
+			(err: any) => {
+				if ( err.code === 1 ) {
+					alert('Favor de activar la geolocalización en tu navegador y recargar la pantalla.');
+				}
+				this.latitud = null;
+				this.longitud = null;
+			}, {timeout: 5000, enableHighAccuracy: true });
+		});
+}
 }
