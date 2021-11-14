1c7bd12e7896023b63b8ec9cfd6cb755130e0764
CessarGarcia <64294314+CessarGarcia@users.noreply.github.com>
2021-11-14 04:03:08 -0600

    Practica 07

--- ionic-basic/src/app/float-menu/float-menu.component.spec.ts
+++ ionic-basic/src/app/float-menu/float-menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
+
 import { FloatMenuComponent } from './float-menu.component';
 
 describe('FloatMenuComponent', () => {
