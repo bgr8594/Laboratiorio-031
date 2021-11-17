5a82340472b76cf559ab4b7e9bd79f0c546b05c1
Ameliones28 <yuviadamaris@gmail.com>
2021-11-16 22:23:29 -0600

    Practica 15 YDVG "Ajuste de font"

--- ionic-basic/src/app/app.module.ts
+++ ionic-basic/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy, NavParams } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,11 +9,15 @@ import { ComponentesModule } from './componentes/componentes.module';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { environment } from 'src/environments/environment';
+import { AuthGuard } from './guards/auth.guard';
+import { FormBuilder, FormsModule, NgControl } from '@angular/forms';  
+import { ReactiveFormsModule} from '@angular/forms'; 
+
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
+  entryComponents: [], 
   imports: [ComponentesModule, BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, AngularFireModule.initializeApp(environment.firebaseConfig), AngularFireAuthModule ],
-  providers: [NavParams, HttpClientModule, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [NavParams, HttpClientModule,AuthGuard,FormBuilder, { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
